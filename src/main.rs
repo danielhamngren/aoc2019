39a104d4f80a60cde9f70ceef56e39a9907fe52b
danielhamngren <daniel.hamngren@gmail.com>
2019-12-01 13:30:51 +0100

    Solution part 2

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,34 @@ use std::io::BufReader;
 
 fn main() {
     let filename = "input";
-    println!("In file {}", filename);
+    let lines: Vec<String> = read_file(filename);
+    let lines_clone = lines.clone();
+    
+    part1(lines);
+    part2(lines_clone);
+}
 
+fn part1(lines: Vec<String>) {
+    println!("Part 1");
+
+    let input = lines.iter().map(|l| l.parse::<i32>().unwrap());
+    let fuel_draw = input.map(|x| calculate_fuel(x));
+    let fuel_draw_total: i32 = fuel_draw.sum();
+
+    println!("Total fuel: {}\n", fuel_draw_total);
+}
+
+fn part2(lines: Vec<String>) {
+    println!("Part 2");
+
+    let input = lines.iter().map(|l| l.parse::<i32>().unwrap());
+    let fuel_draw = input.map(|x| total_fuel(x));
+    let fuel_draw_total: i32 = fuel_draw.sum();
+
+    println!("Total fuel: {}", fuel_draw_total);
+}
+
+fn read_file(filename: &str) -> Vec<String> {
     let f = File::open(filename).expect("file not found");
 
     let reader = BufReader::new(f);
@@ -14,21 +40,22 @@ fn main() {
     // I can use unwrap because I'm sure that every line will be read correctly.
     // I'm basing this on faith alone. Would probably be good to have some
     // additional error checking here.
-    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
-
-    let input = lines.iter().map(|l| l.parse::<i32>().unwrap());
-
-    let fuel_draw = input.map(|x| calculate_fuel(x));
-
-    let fuel_draw_total: i32 = fuel_draw.sum();
-
-    println!("Total fuel: {}", fuel_draw_total);
+    reader.lines().collect::<Result<_, _>>().unwrap()
 }
 
 pub fn calculate_fuel(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
+pub fn total_fuel(mass: i32) -> i32 {
+    let mut total = 0;
+    let mut fuel_mass = calculate_fuel(mass);
+    while fuel_mass > 0 {
+        total += fuel_mass;
+        fuel_mass = calculate_fuel(fuel_mass);
+    }
+    total
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +79,20 @@ mod tests {
     #[test]
     fn mass_test_4() {
         assert_eq!(33583, calculate_fuel(100756));
+    }
+
+    #[test]
+    fn test_total_fuel_1() {
+        assert_eq!(2, total_fuel(12));
+    }
+
+        #[test]
+    fn test_total_fuel_2() {
+        assert_eq!(966, total_fuel(1969));
+    }
+
+        #[test]
+    fn test_total_fuel_3() {
+        assert_eq!(50346, total_fuel(100756));
     }
 }
