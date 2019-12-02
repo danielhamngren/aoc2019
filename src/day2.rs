ceca0acae6348c1318562308298f2835e90ed390
danielhamngren <daniel.hamngren@gmail.com>
2019-12-02 11:39:59 +0100

    solution to day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -5,35 +5,56 @@ use std::io::BufReader;
 pub fn day2() {
     let filename = "resources/day2_input";
     let lines: Vec<String> = read_file(filename);
-    let lines_clone = lines.clone();
     
-    part1(lines);
+    part1(&lines);
+    part2(&lines);
 }
 
-fn part1(lines: Vec<String>) {
+fn part1(lines: &Vec<String>) {
     println!("Day 2: Part 1");
 
     let mut program = preprocessing(lines);
+    set_initial_values(&mut program, 12, 2);
 
     run_intcode(&mut program);
 
     println!("Result part 1: {}", program[0]);
-
-
-    // let input = lines.iter().map(|l| l.parse::<i32>().unwrap());
-    // let fuel_draw = input.map(|x| calculate_fuel(x));
-    // let fuel_draw_total: i32 = fuel_draw.sum();
 }
 
-fn preprocessing(lines: Vec<String>) -> Vec<usize>{
+fn part2(lines: &Vec<String>) {
+    println!("Day 2: Part 1");
+
+    let program_input = preprocessing(lines);
+    let mut program = program_input.clone();
+
+
+    'outer: for noun in 0..99 {
+      'inner: for verb in 0..99 {
+        program = program_input.clone();
+        set_initial_values(&mut program, noun, verb);
+        run_intcode(&mut program);
+        println!("noun {}, verb {}", noun, verb);
+        let stop_criteria = 19690720;
+        if program[0] == 19690720 {
+          println!("{} found, noun is {}, verb is {}", stop_criteria, noun, verb);
+          println!("100 * noun + verb = {}", 100 * noun + verb);
+          break 'outer;
+        }
+      }
+    }
+}
+
+fn preprocessing(lines: &Vec<String>) -> Vec<usize>{
     let mut program = lines[0].split(",").collect::<Vec<&str>>()
                           .iter().map(|l| l.parse::<usize>().unwrap())
                           .collect::<Vec<usize>>();
 
-    program[1] = 12;
-    program[2] = 2;
-
     program
+}
+
+fn set_initial_values(program: &mut Vec<usize>, noun: usize, verb: usize) {
+    program[1] = noun;
+    program[2] = verb;
 }
 
 fn read_file(filename: &str) -> Vec<String> {
@@ -52,10 +73,10 @@ fn run_intcode(intcode: &mut Vec<usize>) {
   let mut done = false;
   let mut pc = 0; // short for program counter
   while !done {
-    println!("pc: {}", pc);
+    // println!("pc: {}", pc);
     match intcode[pc] {
       1 => {
-        println!("Match 1, add");
+        // println!("Match 1, add");
         let result = intcode[intcode[pc+1]] + intcode[intcode[pc+2]];
         let store_address = intcode[pc+3];
         intcode[store_address] = result;
@@ -63,14 +84,14 @@ fn run_intcode(intcode: &mut Vec<usize>) {
 
       },
       2 => {
-        println!("Match 2, multiply");
+        // println!("Match 2, multiply");
         let result = intcode[intcode[pc+1]] * intcode[intcode[pc+2]];
         let store_address = intcode[pc+3];
         intcode[store_address] = result;
         pc += 4;
       },
       99 => {
-        println!{"Match 99, terminate"}
+        // println!{"Match 99, terminate"}
         done = true;
       },
       _ => {
