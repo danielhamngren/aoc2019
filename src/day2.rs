8f7e5f8051d2521ff417b8b1a7e0c9a9d6f5a833
danielhamngren <daniel.hamngren@gmail.com>
2019-12-02 11:17:36 +0100

    Solution day2 part1

--- src/day2.rs
+++ src/day2.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 pub fn day2() {
-    let filename = "resources/day1_input";
+    let filename = "resources/day2_input";
     let lines: Vec<String> = read_file(filename);
     let lines_clone = lines.clone();
     
@@ -13,13 +13,27 @@ pub fn day2() {
 fn part1(lines: Vec<String>) {
     println!("Day 2: Part 1");
 
-    for line in &lines {
-      println!("{}", line);
-    } 
+    let mut program = preprocessing(lines);
+
+    run_intcode(&mut program);
+
+    println!("Result part 1: {}", program[0]);
+
 
     // let input = lines.iter().map(|l| l.parse::<i32>().unwrap());
     // let fuel_draw = input.map(|x| calculate_fuel(x));
     // let fuel_draw_total: i32 = fuel_draw.sum();
+}
+
+fn preprocessing(lines: Vec<String>) -> Vec<usize>{
+    let mut program = lines[0].split(",").collect::<Vec<&str>>()
+                          .iter().map(|l| l.parse::<usize>().unwrap())
+                          .collect::<Vec<usize>>();
+
+    program[1] = 12;
+    program[2] = 2;
+
+    program
 }
 
 fn read_file(filename: &str) -> Vec<String> {
@@ -34,8 +48,38 @@ fn read_file(filename: &str) -> Vec<String> {
     reader.lines().collect::<Result<_, _>>().unwrap()
 }
 
-fn run_intcode(intcode: Vec<i32>) -> Vec<i32> {
-  vec![2,0,0,0,99]
+fn run_intcode(intcode: &mut Vec<usize>) {
+  let mut done = false;
+  let mut pc = 0; // short for program counter
+  while !done {
+    println!("pc: {}", pc);
+    match intcode[pc] {
+      1 => {
+        println!("Match 1, add");
+        let result = intcode[intcode[pc+1]] + intcode[intcode[pc+2]];
+        let store_address = intcode[pc+3];
+        intcode[store_address] = result;
+        pc += 4;
+
+      },
+      2 => {
+        println!("Match 2, multiply");
+        let result = intcode[intcode[pc+1]] * intcode[intcode[pc+2]];
+        let store_address = intcode[pc+3];
+        intcode[store_address] = result;
+        pc += 4;
+      },
+      99 => {
+        println!{"Match 99, terminate"}
+        done = true;
+      },
+      _ => {
+        println!("Match error!");
+      }
+    }
+    // Assume intcodes are correctly written, 
+    // no need to check for reads and writes outside of vector.
+  }
 }
 
 #[cfg(test)]
@@ -44,8 +88,29 @@ mod tests {
 
     #[test]
     fn example_1() {
-        let vec = vec![2,0,0,0,99];
-        assert_eq!(run_intcode(vec), [2,0,0,0,99]);
+        let mut vec = vec![1,0,0,0,99];
+        run_intcode(&mut vec);
+        assert_eq!(vec, [2,0,0,0,99]);
     }
 
+    #[test]
+    fn example_2() {
+        let mut vec = vec![2,3,0,3,99];
+        run_intcode(&mut vec);
+        assert_eq!(vec, [2,3,0,6,99]);
+    }
+
+     #[test]
+    fn example_3() {
+        let mut vec = vec![2,4,4,5,99,0];
+        run_intcode(&mut vec);
+        assert_eq!(vec, [2,4,4,5,99,9801]);
+    }
+
+    #[test]
+    fn example_4() {
+        let mut vec = vec![1,1,1,4,99,5,6,0,99];
+        run_intcode(&mut vec);
+        assert_eq!(vec, [30,1,1,4,2,5,6,0,99]);
+    }
 }
