0042b5efe94836156ebc25aad14f310bc6e4a11e
danielhamngren <daniel.hamngren@gmail.com>
2019-12-04 07:21:17 +0100

    Import of utils is working

--- src/day3.rs
+++ src/day3.rs
@@ -1,35 +1,32 @@
-mod utils;
+use crate::utils;
 
 pub fn day3() {
     let filename = "resources/day3_input";
     let lines: Vec<String> = utils::read_file(filename);
     
     part1(&lines);
-    part2(&lines);
 }
 
 fn part1(lines: &Vec<String>) {
     println!("Day 2: Part 1");
 
-    let vec1 = preprocessing(lines[0]);
-    let vec2 = preprocessing(lines[1]);
+    let (vec1, vec2) = preprocessing(&lines);
 
-    let answer = run(vec1, vec2);
+    let result = run(&vec1, &vec2);
 
-    println!("Result part 1: {}", program[0]);
+    println!("Result part 1: {}", result);
 }
 
-fn run(vec1: &Vec<usize>, vec2: &Vec<usize>) -> i32 {
+fn run(vec1: &Vec<&str>, vec2: &Vec<&str>) -> i32 {
   999
 }
 
-
-fn preprocessing(lines: &Vec<String>) -> Vec<usize>{
+fn preprocessing(lines: &Vec<String>) -> (Vec<&str>, Vec<&str>){
     let mut program = lines[0].split(",").collect::<Vec<&str>>()
                           .iter().map(|l| l.parse::<usize>().unwrap())
                           .collect::<Vec<usize>>();
 
-    program
+    (vec!["999", "tft"], vec!["999", "tff"])
 }
 
 #[cfg(test)]
@@ -41,10 +38,13 @@ mod tests {
         let vec1_input = vec!["R8","U5","L5","D3"];
         let vec2_input = vec!["U7","R6","D4","L4"];
 
-        let vec1 = preprocessing(vec1_input);
-        let vec2 = preprocessing(vec2_input);
+        let mut lines = Vec::new();
+        lines.push(String::from("R8,U5,L5,D3"));
+        lines.push(String::from("U7,R6,D4,L4"));
 
-        let answer = run(vec1, vec2);
+        let (vec1, vec2) = preprocessing(&lines);
+
+        let answer = run(&vec1, &vec2);
         assert_eq!(answer, 6);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,10 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn read_file(filename: &str) -> Vec<String> {
+// terminal_hermit: @namatoj my structure is day/language/[01]/, so i have multiple projects for each day. it's not always the best because the two puzzles of a day might share code, but i don't think you need to share code between puzzles of different days.
+// but if you do want to to share code in your type of structure you can put a pub before every struct/function you want to use in another file, put a mod util; in your main.rs and a use crate::util::somefunction; in a file you want to use a function from util.rs
+
+pub fn read_file(filename: &str) -> Vec<String> {
     let f = File::open(filename).expect("file not found");
 
     let reader = BufReader::new(f);
